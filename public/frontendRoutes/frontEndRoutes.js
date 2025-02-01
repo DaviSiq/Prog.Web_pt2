0021b0001155d0236f905875a3ef1151a9577cb9
Ausslanderr <paciencia.2001@gmail.com>
2025-02-01 11:05:35 -0300

    feat: adicionando arquivo js que faz com que o html encontre os arquivos estaticos da pasta public

---
+++ public/frontendRoutes/frontEndRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+// Servir arquivos estáticos da pasta 'public'
+router.use(express.static('public')); //garante que o html encontre os arquivos estáticos da pasta public
+
+router.get('/', (req, res) => {
+    //Envia um arquivo como resposta, e constrói o caminho absoluto para o arquivo que será enviado.
+    res.sendFile(path.join(__dirname, '..', 'html', 'index.html'));
+
+});
+
+module.exports = router;
